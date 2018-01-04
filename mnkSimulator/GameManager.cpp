f126414a22e3f396b89b7c94681d49f20ffc3f06
Jack Yan <jackyanart@gmail.com>
2018-01-03 21:20:52 -0500

    Add GameManager functions

--- mnkSimulator/GameManager.cpp
+++ mnkSimulator/GameManager.cpp
@@ -8,6 +8,8 @@ EndState GameManager::isTerminal(Board* b)
 	std::vector<std::vector<int>*> * p2 = new std::vector<std::vector<int>*>();
 	int width = b->getNumCols();
 	int height = b->getNumRows();
+	p1->reserve(height);
+	p2->reserve(height);
 	int k = b->getK();
 	bool hasEmpty{false};
 	int connection = 0;
@@ -23,6 +25,11 @@ EndState GameManager::isTerminal(Board* b)
 		p2 = new std::vector<std::vector<int>*>();
 
 		for (int i = 0; i < height; i++) {
+			std::vector<int>* row = new std::vector<int>();
+			row->reserve(width);
+			for (int j = 0; j < width; j++) { row->push_back(0); }
+			p1->push_back(row);
+			p2->push_back(row);
 			for (int j = 0; j < width; j++) {
 				switch(b->getGrid(j,i)) {
 				    case BoardSpace::EMPTY:
@@ -56,7 +63,7 @@ int GameManager::addConnectionToBoard(std::vector<std::vector<int>*> * b, int x,
 	int w = b->at(0)->size();
 	switch(c){
 		case ConnectionDirection::LEFTDIAG:
-			if(x != 0 && y != 0){ b->at(y)->at(x) = b->at(y-1)->at(x-1) + 1; }
+			if(x > 0 && y > 0){ b->at(y)->at(x) = b->at(y-1)->at(x-1) + 1; }
 			else{ b->at(y)->at(x) = 1; }
 			break;
 		case ConnectionDirection::RIGHTDIAG:
@@ -80,13 +87,36 @@ Board* GameManager::simulateMove(Board* b, Move* m, Player* p)
 	//check for terminality first. If b is a terminal state, raise an error.
 	//Also raise error if not valid move.
 	//returns copy of board (use copy constructor)
-	return nullptr;
+
+	if (isTerminal(b) != EndState::NOT_TERMINAL || !isValidMove(b, m, p)) {
+		throw std::bad_exception();
+		return nullptr;
+	}
+
+	b->setGrid(m);
+
+	return new Board(*b);
 }
 
 Moves* GameManager::getValidMoves(Board* b, Player* p)
 {
 	//check for terminality first. If terminal, return empty list.
-	return new Moves{new Move(1,1,1)};
+	Moves * m{};
+
+	if (isTerminal(b) != EndState::NOT_TERMINAL) {
+		return m;
+	}
+
+	for (int i = 0; i < b->getNumRows(); i++)
+	{
+		for (int j = 0; j < b->getNumCols(); j++)
+		{
+			if (b->getGrid(j, i) == BoardSpace::PLAYER2PIECE) {
+				m->push_back(new Move(j, i, p->getPlayerNumber()));
+			}
+		}
+	}
+	return m;
 }
 
 bool GameManager::isValidMove(Board * b, Move * m, Player * p)
@@ -94,5 +124,14 @@ bool GameManager::isValidMove(Board * b, Move * m, Player * p)
 	//No need to check for terminality here. Will be done inside game loop.
 	//check m->isWithinBounds() first
 	//also check if m->getPlayer() == p->getPlayerNumber()
+
+	if (!m->isWithinBounds(b->getNumCols(), b->getNumRows())) return false;
+
+	if (m->getPlayer() != p->getPlayerNumber()) return false;
+
+	if (b->getGrid(m->getX(), m->getY()) == BoardSpace::EMPTY) {
+		return true;
+	}
+
 	return false;
 }
